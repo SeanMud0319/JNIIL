95037a6fd87a4c50e0c68ab59e2be60c74c9b561
SeanMud0319 <whe91000@gmail.com>
2025-12-22 15:01:27 +0800

    減肥

---
+++ src/main/java/top/nontage/jniil/asm/LocalVariableChecker.java
@@ -0,0 +1,41 @@
+package top.nontage.jniil.asm;
+
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.LocalVariableNode;
+import org.objectweb.asm.tree.MethodNode;
+import top.nontage.jniil.utils.InjectionUtil;
+
+import java.io.IOException;
+import java.lang.instrument.UnmodifiableClassException;
+
+public class LocalVariableChecker {
+    public static void printLocalVariableSlot(Class<?> clazz, String methodName, boolean shouldFillLocals) {
+        try {
+            byte[] bytecode;
+            if (shouldFillLocals) {
+                bytecode = new LocalVariableTableFiller().fillLocalVariableNames(clazz, false);
+            } else {
+                bytecode = InjectionUtil.getOriginalClassBytes(clazz);
+            }
+            ClassReader cr = new ClassReader(bytecode);
+            ClassNode cn = new ClassNode();
+            cr.accept(cn, 0);
+            System.out.println("===== LocalVariable Slot For Method: " + methodName + " =====");
+            for (MethodNode mn : cn.methods) {
+                if (!mn.name.equals(methodName)) continue;
+                System.out.println("Method: " + mn.name + mn.desc);
+                if (mn.localVariables != null) {
+                    System.out.println(" Locals:");
+                    for (LocalVariableNode lvn : mn.localVariables) {
+                        System.out.printf("  name=%s, desc=%s, index=%d%n", lvn.name, lvn.desc, lvn.index);
+                    }
+                }
+            }
+            System.out.println("===========================\n");
+        } catch (IOException | UnmodifiableClassException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/top/nontage/jniil/injector/ClassInjector.java
+++ src/main/java/top/nontage/jniil/injector/ClassInjector.java
@@ -1,7 +1,5 @@
 package top.nontage.jniil.injector;
 
-import io.github.classgraph.ClassGraph;
-import io.github.classgraph.ScanResult;
 import top.nontage.auth.library.annotation.Protect;
 import top.nontage.jniil.JNIIL;
 import top.nontage.jniil.annotations.InjectClassInfo;
@@ -10,52 +8,77 @@ import top.nontage.jniil.utils.InjectionUtil;
 
 import java.io.File;
 import java.io.FileOutputStream;
+
 @Protect
 public class ClassInjector {
-    public static void injectAllClass(String packageName) {
-        try (ScanResult scanResult = new ClassGraph()
-                .enableClassInfo()
-                .enableAnnotationInfo()
-                .acceptPackages(packageName)
-                .scan()) {
-            scanResult.getClassesWithAllAnnotations(InjectClassInfo.class.getName())
-                    .forEach(classInfo -> {
-                        System.out.println("Found injectable class: " + classInfo.getName());
-                        try {
-                            Class<?> clazz = classInfo.loadClass();
-                            if (Injectable.class.isAssignableFrom(clazz)) {
-                                InjectClassInfo info = clazz.getAnnotation(InjectClassInfo.class);
-                                if (info == null) return;
-                                String anchorClassName = info.anchorClass();
-                                String injectClassName = clazz.getName();
-                                String anchorThreadName = info.anchorThread();
-                                ClassLoader targetLoader;
-                                if (anchorThreadName == null || anchorThreadName.isEmpty()) {
-                                    targetLoader = InjectionUtil.findClassAcrossClassLoaders(anchorClassName).getClassLoader();
-                                } else {
-                                    targetLoader = InjectionUtil.findClassLoaderByThread(anchorThreadName);
-                                }
-                                byte[] originalBytecode = InjectionUtil.getOriginalClassBytes(clazz);
-                                InjectionUtil.unsafeInjectClass(targetLoader, injectClassName, originalBytecode);
-                                System.out.println("Injected " + injectClassName + " into " + anchorClassName + anchorThreadName);
-                                if (JNIIL.isClassOutputEnabled()) {
-                                    File outputDir = JNIIL.getClassOutputDir();
-                                    if (!outputDir.exists()) outputDir.mkdirs();
-                                    String filePath = injectClassName.replace('.', '/') + ".class";
-                                    File outputFile = new File(outputDir, filePath);
-                                    outputFile.getParentFile().mkdirs();
-                                    try (FileOutputStream fos = new FileOutputStream(outputFile)) {
-                                        fos.write(originalBytecode);
-                                        fos.flush();
-                                        System.out.println("Dumped injected class to: " + outputFile.getAbsolutePath());
-                                    }
-                                }
-                            }
-                        } catch (Throwable e) {
-                            e.printStackTrace();
 
-                        }
-                    });
+    public static void injectClasses(Class<?>... classes) {
+        for (Class<?> clazz : classes) {
+            injectClass(clazz);
         }
+    }
+
+    public static void injectClass(Class<?> clazz) {
+        try {
+            if (!Injectable.class.isAssignableFrom(clazz)) {
+                return;
+            }
+
+            InjectClassInfo info = clazz.getAnnotation(InjectClassInfo.class);
+            if (info == null) {
+                return;
+            }
+
+            String anchorClassName = info.anchorClass();
+            String injectClassName = clazz.getName();
+            String anchorThreadName = info.anchorThread();
+
+            ClassLoader targetLoader;
+
+            if (anchorThreadName == null || anchorThreadName.isEmpty()) {
+                targetLoader = InjectionUtil
+                        .findClassAcrossClassLoaders(anchorClassName)
+                        .getClassLoader();
+            } else {
+                targetLoader = InjectionUtil.findClassLoaderByThread(anchorThreadName);
+            }
+
+            byte[] originalBytecode = InjectionUtil.getOriginalClassBytes(clazz);
+
+            InjectionUtil.unsafeInjectClass(
+                    targetLoader,
+                    injectClassName,
+                    originalBytecode
+            );
+
+            System.out.println(
+                    "Injected " + injectClassName +
+                            " into " + anchorClassName +
+                            (anchorThreadName == null ? "" : ("@" + anchorThreadName))
+            );
+
+            if (JNIIL.isClassOutputEnabled()) {
+                dumpClass(injectClassName, originalBytecode);
+            }
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void dumpClass(String className, byte[] bytecode) throws Exception {
+        File outputDir = JNIIL.getClassOutputDir();
+        if (!outputDir.exists()) outputDir.mkdirs();
+
+        String filePath = className.replace('.', '/') + ".class";
+        File outputFile = new File(outputDir, filePath);
+        outputFile.getParentFile().mkdirs();
+
+        try (FileOutputStream fos = new FileOutputStream(outputFile)) {
+            fos.write(bytecode);
+            fos.flush();
+        }
+
+        System.out.println("Dumped injected class to: " + outputFile.getAbsolutePath());
     }
 }

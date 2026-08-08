3d4928f40ec6dca1ab11dfbc8e9db93cda0b25a1
SeanMud0319 <whe91000@gmail.com>
2026-08-08 23:45:40 +0800

    docs: Wrong instruct.

--- src/main/java/top/nontage/jniil/annotations/Overwrite.java
+++ src/main/java/top/nontage/jniil/annotations/Overwrite.java
@@ -8,8 +8,9 @@ import java.lang.annotation.Target;
 /**
  * Marks a method to completely overwrite the target method's body.
  * <p>
- * <b>Note:</b> This will clear and replace the original target instructions entirely.
- * Applicable to all injectors.
+ * <b>Note:</b> This annotation is only available for
+ * {@link top.nontage.jniil.injector.StandardMethodInjector} and
+ * {@link top.nontage.jniil.injector.functional.FunctionalInjector}.
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
